8a480ba572f2fb76c6a5d11195b1bdd6128284ef
Chris Clarkson <c.clarkson@ucl.ac.uk>
2026-03-12 21:27:34 +0000

    simplified

--- run_sample.sh
+++ run_sample.sh
@@ -12,4 +12,39 @@ R2=$(echo $LINE | awk '{print $3}')
 
 echo "Running sample: $SAMPLE"
 
-bash pipeline.sh $SAMPLE $R1 $R2 $ALIGNER $CALLER
+
+if [ "$ALIGNER" = "bwa" ]; then
+    echo "Running BWA-MEM alignment"
+    bwa mem -t $THREADS $REF $R1 $R2 | samtools sort -@ $THREADS -o ${SAMPLE}.bam
+    gatk MarkDuplicates \
+        -I ${SAMPLE}.bam \
+        -O ${SAMPLE}.dedup.bam \
+        -M ${SAMPLE}.metrics.txt
+    samtools index ${SAMPLE}.dedup.bam
+elif [ "$ALIGNER" = "minimap2" ]; then
+    echo "Running minimap2 alignment"
+    minimap2 -ax sr -t $THREADS $REF $R1 $R2 | samtools sort -@ $THREADS -o ${SAMPLE}.bam
+fi
+
+samtools index ${SAMPLE}.bam
+
+if [ "$CALLER" = "gatk" ]; then
+    echo "Running GATK HaplotypeCaller"
+
+    gatk HaplotypeCaller \
+        -R $REF \
+        -I ${SAMPLE}.dedup.bam \
+        -O ${SAMPLE}.g.vcf.gz \
+        -ERC GVCF
+
+elif [ "$CALLER" = "deepvariant" ]; then
+    echo "Running DeepVariant"
+
+    run_deepvariant \
+        --model_type=WGS \
+        --ref=$REF \
+        --reads=${SAMPLE}.dedup.bam \
+        --output_vcf=${SAMPLE}.vcf.gz \
+        --num_shards=$THREADS
+fi
+
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=wgs_pipeline
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
+#SBATCH --job-name=example
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
 #SBATCH --time=24:00:00
-#SBATCH --array=1-100
 
-bash run_sample.sh
+bash run_sample.sh $1 $2 $3
